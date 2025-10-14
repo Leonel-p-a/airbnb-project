b23eeb19216b1e94173c85fceed655a65ca27546
Leonel Almeida <leonelalmeida.10.la@gmail.com>
2025-10-14 10:21:27 -0300

    :sparkles: feat: Criação do botão de entrar

--- src/widgets/TopBar.tsx
+++ src/widgets/TopBar.tsx
@@ -1,19 +1,32 @@
-import Logo from "@/components/Logo/Logo"
+// import { IconUserCircle } from "@/assets/icons";
+import { IconUserCircle } from "@/assets/icons";
+import ButtonIcon from "@/components/ButtonIcon";
+import Logo from "@/components/Logo"
 import Link from "next/link"
 
 const TopBar = () => {
-    return(
-        <div className="container py-6 mx-auto flex justify-around items-center">
-          <Logo />
+	return (
+		<div className="container py-6 mx-auto flex justify-around items-center">
+			<Logo />
 
-          <div className="flex gap-6">
-            <Link className="font-semibold" href="/">Acomodações</Link>
-            <Link className="opacity-60" href="/">Experiências</Link>
-          </div>
+			<div className="flex gap-6">
+				<Link className="font-semibold" href="/">Acomodações</Link>
+				<Link className="opacity-60" href="/">Experiências</Link>
+			</div>
 
-          <button className="rounded-md border-2">Entrar</button>
-        </div>
-    )
+			<ButtonIcon
+				icon={(
+					<IconUserCircle
+						aria-label="Ícone de usuário"
+						size={20}
+					/>
+				)} >
+				Entrar
+			</ButtonIcon>
+
+
+		</div>
+	)
 }
 
 export default TopBar;
