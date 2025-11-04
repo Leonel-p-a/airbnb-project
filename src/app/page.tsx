ce7c119de10997493fdde826eb489689eedcd2e6
Leonel Almeida <leonelalmeida.10.la@gmail.com>
2025-11-03 21:26:01 -0300

    :sparkles: feat: Icon Slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
+import NavigationTab from "@/widgets/NavigationTab";
 import SearchBar from "@/widgets/SearchBar";
 import TopBar from "@/widgets/TopBar";
 
 export default function Home() {
-  return (
-    <>
-      <div>
-        <TopBar />
-        <SearchBar />
-      </div>
-      <hr className="mt-5 text-gray-200" />
-      <div className="border-orange-700 border-3 mt-10">Área conteúdos</div>
-      <div className="border-orange-900 border-3">Área rodapé</div>
-    </>
-  );
+    return (
+        <>
+            <header className="container lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl mx-auto py-6 px-3">
+                <TopBar />
+                <SearchBar />
+            </header>
+
+            <hr className="my-3 text-gray-200" />
+
+            <main className="container mx-auto px-3">
+                <NavigationTab />
+            </main>
+
+            <footer className="container max-w-7xl mx-auto px-3">
+                
+            </footer>
+        </>
+    );
 }
--- src/widgets/TopBar.tsx
+++ src/widgets/TopBar.tsx
@@ -6,13 +6,13 @@ import Link from "next/link"
 
 const TopBar = () => {
 	return (
-		<div className="container py-6 mx-auto flex justify-around items-center">
+		<div className="flex justify-between items-center">
 			<Logo />
 
-			<div className="flex gap-6">
+			<nav className="flex gap-6">
 				<Link className="font-semibold" href="/">Acomodações</Link>
 				<Link className="opacity-60" href="/">Experiências</Link>
-			</div>
+			</nav>
 
 			<ButtonIcon
 				icon={(
